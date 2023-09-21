f763a33a984138773a7cd9f1692166938ba9a4ba
Muhammet Emre Vatan <emre.vatan98@hotmail.com>
2023-09-21 19:41:00 +0300

    Initial commit

---
+++ src/main/java/com/memrevatan/webminder/WebMinderApplication.java
@@ -0,0 +1,13 @@
+package com.memrevatan.webminder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebMinderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebMinderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/memrevatan/webminder/WebMinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.memrevatan.webminder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebMinderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
